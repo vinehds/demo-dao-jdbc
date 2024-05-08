abe01b93041ace6f01849e3514583e104191241a
vinehds <viniciushdalaqua@gmail.com>
2024-05-08 11:37:25 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Seller id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
